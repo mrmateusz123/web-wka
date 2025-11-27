90c08635d1c3d9af65839e9e3ace52c173b293e4
mr. mateusz <matidodo131@wp.pl>
2025-11-27 14:36:14 +0100

    Create input.js

---
+++ kw_zadanie/src/components/input.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+const Input = props =>{
+    const {vA, vB, vC} = props;
+    return(
+        <div>
+            
+        </div>
+    )
+}
+Input.propTypes = {
+    vA: PropTypes.string,
+    vB: PropTypes.string,
+    vC: PropTypes.string
+};
+export default Input;
